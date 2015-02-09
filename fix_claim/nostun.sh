0f66c08f01058238b72a6c453588f367a6b655b0
Fredrik Stromberg <stromberg@insto.org>
2015-02-09 17:45:38 +0100

    Add Linux counterpart to nostun.txt

---
+++ fix_claim/nostun.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+iptables -A OUTPUT -d 54.172.47.69 -j REJECT
+iptables -A OUTPUT -d 173.194.65.127 -j REJECT
+iptables -A OUTPUT -d 173.194.67.127 -j REJECT
+iptables -A OUTPUT -d 173.194.68.127 -j REJECT
+iptables -A OUTPUT -d 173.194.71.127 -j REJECT
+iptables -A OUTPUT -d 173.194.72.127 -j REJECT
+iptables -A OUTPUT -d 173.194.73.127 -j REJECT
+iptables -A OUTPUT -d 173.194.78.127 -j REJECT
+iptables -A OUTPUT -d 74.125.131.127 -j REJECT
+iptables -A OUTPUT -d 74.125.136.127 -j REJECT
+iptables -A OUTPUT -d 74.125.142.127 -j REJECT
+iptables -A OUTPUT -d 74.125.200.127 -j REJECT
